38e0bb13505a55a52799f2d1266f390a7d8083a3
daniel.gasson <daniel.gasson@cots.local>
2017-06-15 17:04:36 +0100

    Put an email into azure table storage

---
+++ Notifications/Notifications.EmailProcessor/CustomerEmail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Notifications.EmailProcessor
+{
+	public class CustomerEmail : TableEntity
+	{
+		public CustomerEmail(int customerId, string content)
+		{
+			this.PartitionKey = customerId.ToString();
+			this.RowKey = Guid.NewGuid().ToString();
+
+			CustomerId = customerId;
+			Content = content;
+			CreatedDate = DateTime.Now;
+		}
+
+		public CustomerEmail()
+		{
+		}
+
+		public int CustomerId { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public string Content { get; set; }
+	}
+}
--- Notifications/Notifications.EmailProcessor/Program.cs
+++ Notifications/Notifications.EmailProcessor/Program.cs
@@ -1,15 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Net;
+using Microsoft.Azure;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Notifications.EmailProcessor
 {
 	class Program
 	{
+		private const string TableName = "CustomerEmails";
+		private const string EmailContent = "Hi, your monthly statement is ready.";
+
 		static void Main(string[] args)
 		{
+			try
+			{
+				// storage client setup
+				var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+				var storageClient = storageAccount.CreateCloudTableClient();
+				CreateTableIfExists(storageClient, TableName);
+
+				// queue client setup
+				var queueConnectionString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+				var queueName = "emailgeneratorqueue";
+				var queueClient = QueueClient.CreateFromConnectionString(queueConnectionString, queueName);
+
+				var options = new OnMessageOptions
+				{
+					AutoComplete = true
+				};
+				options.ExceptionReceived += (sender, eventArgs) => { Console.WriteLine(eventArgs.Exception.Message); };
+
+				queueClient.OnMessage(message =>
+				{
+					var customerId = message.Properties["customerId"];
+					var firstName = message.Properties["firstName"];
+					var lastName = message.Properties["lastName"];
+
+					var customerEmail = new CustomerEmail
+					(
+						customerId: (int) customerId,
+						content: EmailContent
+					);
+
+					var success = AddEntityToStorage(storageClient, customerEmail, TableName);
+					if (success)
+					{
+						QueueMsg(customerId, firstName, lastName);
+					}
+				}, options);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		private static bool CreateTableIfExists(CloudTableClient client, string tableName)
+		{
+			var table = client.GetTableReference(TableName);
+			return table.CreateIfNotExists();
+		}
+
+		private static bool AddEntityToStorage(CloudTableClient client, ITableEntity entity, string tableName)
+		{
+			var table = client.GetTableReference(tableName);
+			var result = table.Execute(TableOperation.Insert(entity));
+			return result.HttpStatusCode == (int) HttpStatusCode.NoContent;
+		}
+
+		private static void QueueMsg(object customerId, object firstName, object lastName)
+		{
+			var connectionString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+			var pdfQueueName = "smssenderqueue";
+			var client = QueueClient.CreateFromConnectionString(connectionString, pdfQueueName);
+
+			var message = new BrokeredMessage();
+			message.Properties.Add("customerId", customerId);
+			message.Properties.Add("firstName", firstName);
+			message.Properties.Add("lastName", lastName);
+
+			client.Send(message);
 		}
 	}
 }
