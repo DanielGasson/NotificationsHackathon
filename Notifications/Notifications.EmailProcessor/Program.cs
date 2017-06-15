d68787533cd0f5c194cc8e35432cb7a8e7174863
daniel.gasson <daniel.gasson@cots.local>
2017-06-15 13:59:16 +0100

    Email Processor

---
+++ Notifications/Notifications.EmailProcessor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifications.EmailProcessor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
