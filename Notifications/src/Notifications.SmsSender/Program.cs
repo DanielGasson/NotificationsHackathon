db7b62ca244e91323149204c7b11e2fb314db876
daniel.gasson <daniel.gasson@cots.local>
2017-06-15 17:57:45 +0100

    Send Sms skeleton

---
+++ Notifications/src/Notifications.SmsSender/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using RestSharp;
+
+namespace Notifications.SmsSender
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SendSms("", "This is a trial run");
+		}
+
+		private static void SendSms(string number, string content)
+		{
+			try
+			{
+				var apiKey = ConfigurationManager.AppSettings["TextLocalApiKey"];
+				var request = new RestRequest("send", Method.POST);
+				request.AddParameter("sender", "Dan and Sunit");
+				request.AddParameter("message", content);
+				request.AddParameter("apiKey", apiKey);
+				request.AddParameter("numbers", number);
+
+				var client = new RestClient("https://api.txtlocal.com");
+				var response = client.Execute(request);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Failed to send text message. Details: {ex.Message}");
+			}
+		}
+	}
+}
