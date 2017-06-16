479318892093ddede6c74f96cf702ff2684c551a
daniel.gasson <daniel.gasson@cots.local>
2017-06-16 12:09:06 +0100

    Load and dismiss individual messages

--- Notifications/src/Notifications.Web/Controllers/NotificationsController.cs
+++ Notifications/src/Notifications.Web/Controllers/NotificationsController.cs
@@ -11,10 +11,10 @@ namespace Notifications.Web.Controllers
 		// mimic db result until I hook up azure storage
 		private readonly List<CustomerMessageViewModel> _messages = new List<CustomerMessageViewModel>
 		{
-			new CustomerMessageViewModel { Title = "Test 1", Body = "Body for test 1", RowKey = Guid.NewGuid() },
-			new CustomerMessageViewModel { Title = "Test 2", Body = "Body for test 2", RowKey = Guid.NewGuid() },
-			new CustomerMessageViewModel { Title = "Test 3", Body = "Body for test 3", RowKey = Guid.NewGuid() },
-			new CustomerMessageViewModel { Title = "Test 4", Body = "Body for test 4", RowKey = Guid.NewGuid() }
+			new CustomerMessageViewModel { Title = "Test 1", Body = "Body for test 1", RowKey = new Guid("BE61B744-BB8E-440B-8EDB-872045E136FA") },
+			new CustomerMessageViewModel { Title = "Test 2", Body = "Body for test 2", RowKey = new Guid("AD39361E-0532-42D2-B0CA-0ED17DBAAA09") },
+			new CustomerMessageViewModel { Title = "Test 3", Body = "Body for test 3", RowKey = new Guid("60404F64-9E81-4604-933C-6471A5E5A2D5") },
+			new CustomerMessageViewModel { Title = "Test 4", Body = "Body for test 4", RowKey = new Guid("51EDE11D-C373-48EF-A686-64B06E30F79B") }
 		};
 
 		[HttpGet]
--- Notifications/src/Notifications.Web/Scripts/notifications/notifications.js
+++ Notifications/src/Notifications.Web/Scripts/notifications/notifications.js
@@ -1,15 +1,8 @@
 ﻿$(document).ready(function() {
 	$('.table > tbody > tr').click(function () {
 		var id = $(this).find('td').attr('id');
-		//window.alert('Row was clicked with id ' + id);
-
-		$.ajax({
-			type: "GET",
-			url: "/Notifications/Message",
-			data: id,
-			success: function(result) {
-				//window.alert('Row was clicked with id ' + id);
-			}
+		$.get("/Notifications/Message", { id: id }, function(data) {
+			$(document).find('#messageContainer').replaceWith(data);
 		});
 	});
 });
