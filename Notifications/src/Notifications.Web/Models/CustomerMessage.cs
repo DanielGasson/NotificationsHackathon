4a369d27ba9554291324650e7c39419aa65f06c2
daniel.gasson <daniel.gasson@cots.local>
2017-06-16 10:43:20 +0100

    Add page to view all messages, and invididual message

--- Notifications/src/Notifications.Web/Models/CustomerMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Notifications.Web.Models
-{
-	public class CustomerMessage
-	{
-		public int CustomerId { get; set; }
-		public string Title { get; set; }
-		public string Body { get; set; }
-	}
-}
---
+++ Notifications/src/Notifications.Web/ViewModels/CustomerMessageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Notifications.Web.ViewModels
+{
+	public class CustomerMessageViewModel
+	{
+		public int CustomerId { get; set; }
+		public Guid RowKey { get; set; }
+		public string Title { get; set; }
+		public string Body { get; set; }
+	}
+}
