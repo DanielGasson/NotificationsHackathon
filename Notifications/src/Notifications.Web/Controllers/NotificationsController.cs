384be62a273e47a01308be0f76e0ca1b035d498f
daniel.gasson <daniel.gasson@cots.local>
2017-06-16 08:09:54 +0100

    Add Inbox view

---
+++ Notifications/src/Notifications.Web/Controllers/NotificationsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Notifications.Web.Controllers
+{
+	public class NotificationsController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
