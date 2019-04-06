ff8456d9af2926d8fff191611f68a88c3da7f33d
Daniel Gasson <daniel.gasson@gmail.com>
2019-04-06 14:45:43 +0100

    Remove bootstrap
    NB: Haven't tested the implications of removing this. The site probably won't work anymore

--- Notifications/src/Notifications.Web/App_Start/BundleConfig.cs
+++ Notifications/src/Notifications.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Notifications.Web
 {
@@ -18,14 +17,6 @@ namespace Notifications.Web
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
-
-			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js",
-					  "~/Scripts/respond.js"));
-
-			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
 		}
 	}
 }
